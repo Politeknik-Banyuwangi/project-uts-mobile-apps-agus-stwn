825c297f1cb2e4262d6f83008291160cae1a8205
FlutLab.io <>
2022-05-31 09:00:22 +0000

    Agus

--- lib/calls.dart
+++ lib/calls.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'Widget/CardCalls.dart';
 
 class Calls extends StatelessWidget {
   const Calls({Key? key}) : super(key: key);
@@ -18,11 +19,28 @@ class Calls extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
+            CardsCalls('homan', 'Kemarin 07.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Tayo', 'Kemarin 07.30', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Tiyul', 'Kemarin 08.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Mala', 'Kemarin 09.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Luffy', 'Kemarin 10.00', 'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/02/05/4196335095.jpg', 'vidio call', 'accept'),
+            CardsCalls('Zoro', 'Kemarin 11.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Metha', 'Kemarin 13.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Lala', 'Kemarin 15.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Wawan', 'Kemarin 19.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Acil', 'Kemarin 20.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Acil', 'Kemarin 21.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Riri', 'Kemarin 21.30', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Riri', 'hari ini 03.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Rama', 'hari ini 04.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Rama', 'hari ini 05.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Rima', 'hari ini 06.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Rima', 'hari ini 09.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Rima', 'hari ini 11.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Ian', 'hari ini 15.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
+            CardsCalls('Ian', 'hari ini 21.00', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg', 'vidio call', 'accept'),
 
             // Masukkan Coding disini jangan rubah lainnya
-            //
-            //
-            //
             //
             //
             //
--- lib/chats.dart
+++ lib/chats.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'Widget/CardChats.dart';
 
 class Chats extends StatelessWidget {
   const Chats({Key? key}) : super(key: key);
@@ -17,6 +18,27 @@ class Chats extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
+            CardChats('Rima', '01.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Royan', '02.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Maca', '02.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Laro', '03.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('hotman', '04.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Luffy', '05.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Cika', '05.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Kaja', '06.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Kadek', '07.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Maya', '08.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Zoro', '08.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Maria', '09.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Zoro', '09.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Dora', '09.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Abdel', '09.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Paijo', '11.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Panjul', '11.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Rama', '11.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Raka', '13.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('Rahmat', '13.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
+            CardChats('dhoni', '14.00', 'P', 'https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg'),
           ],
         ),
       ),
